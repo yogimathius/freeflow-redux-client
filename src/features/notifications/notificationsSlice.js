ed8365dc82faa718611ec4750e1662b26c464ca6
Mathius Johnson <devachievem@gmail.com>
2020-11-13 21:55:37 -0800

    fixed a few errors

--- src/features/notifications/notificationsSlice.js
+++ src/features/notifications/notificationsSlice.js
@@ -1,54 +1,54 @@
-import {
-  createSlice,
-  createAsyncThunk,
-  createEntityAdapter,
-} from '@reduxjs/toolkit'
+// import {
+//   createSlice,
+//   createAsyncThunk,
+//   createEntityAdapter,
+// } from '@reduxjs/toolkit'
 
-// import { client } from '../../api/client'
-import { api } from '../../.api/index'
+// // import { client } from '../../api/client'
+// import { api } from '../../.api/index'
 
-const notificationsAdapter = createEntityAdapter({
-  sortComparer: (a, b) => b.date.localeCompare(a.date),
-})
+// const notificationsAdapter = createEntityAdapter({
+//   sortComparer: (a, b) => b.date.localeCompare(a.date),
+// })
 
-export const fetchNotifications = createAsyncThunk(
-  'notifications/fetchNotifications',
-  async (_, { getState }) => {
-    const allNotifications = selectAllNotifications(getState())
-    const [latestNotification] = allNotifications
-    const latestTimestamp = latestNotification ? latestNotification.date : ''
-    const response = await api.get(
-      `/fakeApi/notifications?since=${latestTimestamp}`
-    )
-    return response.notifications
-  }
-)
+// export const fetchNotifications = createAsyncThunk(
+//   'notifications/fetchNotifications',
+//   async (_, { getState }) => {
+//     const allNotifications = selectAllNotifications(getState())
+//     const [latestNotification] = allNotifications
+//     const latestTimestamp = latestNotification ? latestNotification.date : ''
+//     const response = await api.get(
+//       `/fakeApi/notifications?since=${latestTimestamp}`
+//     )
+//     return response.notifications
+//   }
+// )
 
-const notificationsSlice = createSlice({
-  name: 'notifications',
-  initialState: notificationsAdapter.getInitialState(),
-  reducers: {
-    allNotificationsRead(state, action) {
-      Object.values(state.entities).forEach((notification) => {
-        notification.read = true
-      })
-    },
-  },
-  extraReducers: {
-    [fetchNotifications.fulfilled]: (state, action) => {
-      Object.values(state.entities).forEach((notification) => {
-        // Any notifications we've read are no longer new
-        notification.isNew = !notification.read
-      })
-      notificationsAdapter.upsertMany(state, action.payload)
-    },
-  },
-})
+// const notificationsSlice = createSlice({
+//   name: 'notifications',
+//   initialState: notificationsAdapter.getInitialState(),
+//   reducers: {
+//     allNotificationsRead(state, action) {
+//       Object.values(state.entities).forEach((notification) => {
+//         notification.read = true
+//       })
+//     },
+//   },
+//   extraReducers: {
+//     [fetchNotifications.fulfilled]: (state, action) => {
+//       Object.values(state.entities).forEach((notification) => {
+//         // Any notifications we've read are no longer new
+//         notification.isNew = !notification.read
+//       })
+//       notificationsAdapter.upsertMany(state, action.payload)
+//     },
+//   },
+// })
 
-export const { allNotificationsRead } = notificationsSlice.actions
+// export const { allNotificationsRead } = notificationsSlice.actions
 
-export default notificationsSlice.reducer
+// export default notificationsSlice.reducer
 
-export const {
-  selectAll: selectAllNotifications,
-} = notificationsAdapter.getSelectors((state) => state.notifications)
+// export const {
+//   selectAll: selectAllNotifications,
+// } = notificationsAdapter.getSelectors((state) => state.notifications)
