16a39ae86f5fe0b4bd5c57d4b6ce106b8ed3ae78
Mathius Johnson <devachievem@gmail.com>
2021-01-28 13:21:24 -0800

    create axios post for user login

--- src/features/login/userLoginSlice.js
+++ src/features/login/userLoginSlice.js
@@ -1,5 +1,9 @@
 import { createSlice } from '@reduxjs/toolkit'
+import axios from 'axios';
 // Slice
+const url = 'https://freeflow-two-point-o.herokuapp.com/api/login-real'
+
+
 const initialUser = localStorage.getItem('user')
   ? JSON.parse(localStorage.getItem('user'))
   : null
@@ -26,8 +30,11 @@ export default userLoginSlice.reducer
 const { loginSuccess, logoutSuccess } = userLoginSlice.actions
 export const login = ({ username, password }) => async dispatch => {
   try {
-    // const res = await api.post('/api/auth/login/', { username, password })
-    dispatch(loginSuccess({username}));
+    const res = await axios.post(url, { username, password })
+    console.log(res);
+    if (res.status === 200) {
+      dispatch(loginSuccess({username}));
+    }
   } catch (e) {
     return console.error(e.message);
   }
