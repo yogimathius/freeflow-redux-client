68a72fe2fc1ae0461eda2846ad7f133f5ff8feaf
Mathius Johnson <devachievem@gmail.com>
2021-02-01 16:09:48 -0800

    cleanup commentslist  for unused code/warnings

--- src/features/comments/CommentsList.js
+++ src/features/comments/CommentsList.js
@@ -1,12 +1,8 @@
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-// import { formatDistanceToNow, parseISO } from 'date-fns'
-import classnames from 'classnames'
-
 import { selectAllUsers } from '../users/usersSlice'
 import UserImage from '../users/UserImage'
 import { fetchComments, selectCommentsByPostId } from './commentsSlice'
-import { selectAllExperiences } from '../experiences/experiencesSlice'
 
 export const CommentsList = ({ postId }) => {
   const comments = useSelector((state) => selectCommentsByPostId(state, parseInt(postId)))
@@ -33,23 +29,12 @@ export const CommentsList = ({ postId }) => {
   
   console.log(content, postId);
   const users = useSelector(selectAllUsers)
-  const karmas = useSelector(selectAllExperiences)
   
   const renderedComments = comments.map((comment) => {
-    // const date = parseISO(comment.time_posted)
-    // const timeAgo = formatDistanceToNow(date)
 
     const user = users.find((user) => user.id === comment.commenter_id) || {
       name: 'Unknown User',
     }
-
-    const commentClassname = classnames('comment', {
-      new: comment.isNew,
-    })
-
-    const commentKarmas = karmas.filter(karma => karma.comment_id === comment.id)
-
-    const commentExperience = (commentKarmas.length * 29);
 
     return (
       <div key={comment.id} className="flex">
