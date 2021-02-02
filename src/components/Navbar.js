6cc0d21a6289bc2f040a6ee64966a38fe049bab6
Mathius Johnson <devachievem@gmail.com>
2021-02-01 20:50:05 -0800

    add styles to login page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -22,13 +22,13 @@ export const Navbar = () => {
     <nav className="pt-3 pb-1 mb-4 bg-green-500">
       <section className="grid grid-cols-4 grid-rows-2 ">
         <div className="col-span-4 flex justify-center ">
-          <h1 className="text-white">Freeflow</h1>
+          <h1 className="text-white text-2xl font-extrabold">Freeflow Social Network</h1>
         </div>
 
         <div className="row-start-2 col-span-4 grid grid-cols-4 my-2 space-x-2 font-bold items-end">
           <Link className="text-white col-start-1 flex justify-center" to="/dashboard">Posts</Link>
           <Link className="text-white col-start-2 flex justify-center" to="/users">Users</Link>
-          <Link className="text-white col-start-3 flex justify-center" to={`/userprofile/${user.id}`}>Profile</Link>
+          <Link className="text-white col-start-3 flex justify-center" to={`/userprofile/${user?.id}`}>Profile</Link>
           <div className="text-white col-start-4 flex justify-center">
           {!user  ?
             <Link to="/login">Login</Link>
--- src/features/login/LoginPage.js
+++ src/features/login/LoginPage.js
@@ -15,17 +15,20 @@ export default function LoginPage() {
   }
 
   return (
-    <div>
+    <div className="text-center space-y-3">
       <h1>Please login to view content.</h1>
       <Formik
+        className="flex justify-center"
         initialValues={{ username: '', password: '' }}
         onSubmit={(values) => { dispatch(login(values)) }}
       >
         {({ isSubmitting }) => (
-          <Form>
-            <Field type="text" name="username" />
-            <Field type="password" name="password" />
-            <button type="submit" disabled={isSubmitting}>Login</button>
+          <Form className="flex flex-col w-2/3 md:w-1/4 mx-auto space-y-3">
+            <Field className="rounded-xl py-1 border-1 border-gray-400" placeholder="Username" type="text" name="username" />
+            <Field className="rounded-xl py-1 border-1 border-gray-400" placeholder="Password" type="password" name="password" />
+            <div className="flex justify-center">
+              <button className="btn btn-primary" type="submit" disabled={isSubmitting}>Login</button>
+            </div>
           </Form>
         )}
       </Formik>
