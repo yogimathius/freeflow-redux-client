142cffd621ebf699711a12c943a0685ba207bd99
Mathius <devachievem@gmail.com>
2021-03-24 11:23:20 -0700

    add tailwind classes being purged

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,10 @@ import React from 'react';
 const Footer = () => {
 	return (
 		<div className="h-12 bg-green-500">
-			
+			<div className="text-yellow-500"></div>
+			<div className="text-purple-500"></div>
+			<div className="text-blue-500"></div>
+			<div className="text-red-500"></div>
 		</div>
 	);
 };
