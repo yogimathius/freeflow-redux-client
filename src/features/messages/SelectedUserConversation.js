dfcf278b2bc17b48711c3fde5c8ceb0a5b32e097
Mathius <devachievem@gmail.com>
2021-06-30 22:33:52 -0700

    add usermessagedetail to selected user conv

--- src/features/messages/SelectedUserConversation.js
+++ src/features/messages/SelectedUserConversation.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import UserMessageDetail from './UserMessageDetail';
 
-const SelectedUserConversation = () => {
+const SelectedUserConversation = ({sortedMessages}) => {
     let { messagerId } = useParams();
 
+    const userMessages = sortedMessages.messages[messagerId]
 
+    const renderedMessages = userMessages.map((message, index) => {
+        return (
+            <UserMessageDetail key={index} message={message} />
+        )
+    })
     return (
-        <div>
-            From router: {messagerId}
+        <div className="col-span-2">
+            {renderedMessages}
         </div>
     );
 };
