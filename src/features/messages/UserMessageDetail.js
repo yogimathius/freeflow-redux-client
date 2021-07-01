f76b572cea5d5dfef6e802c37f4b6083878c4879
Mathius <devachievem@gmail.com>
2021-06-30 22:34:17 -0700

    create usermessage detail

---
+++ src/features/messages/UserMessageDetail.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { saveState } from '../../helpers/localStorage';
+import { TimeAgo } from '../posts/TimeAgo';
+import useVisualMode from '../../hooks/useVisualMode'
+import UserImage from '../users/UserImage';
+import { selectUserById } from '../users/usersSlice';
+
+const SHOW = "SHOW";
+// const CONFIRM = "CONFIRM";
+// const SAVING = "SAVING";
+const EDITING = "EDITING";
+// const ERROR_SAVE = "ERROR_SAVE";
+// const ERROR_DELETE = "ERROR_DELETE";
+
+const UserMessageDetail = ({message, userId}) => {
+    
+    const user = useSelector((state) => selectUserById(state, message.senderid))
+
+    const { mode, transition } = useVisualMode(SHOW);
+
+
+    function onEdit() {
+        transition(EDITING);
+    }
+    
+    function onSaveEdit() {
+        transition(SHOW);
+    }
+
+    return (
+        <div key={message.id} className="bg-white  mx-1 border-2 border-solid border-green-500 border-opacity-25 my-2 rounded-xl">
+        <div className="p-3">
+          <div className="flex justify-between">
+            <Link to={`/userprofile/${user.id}`} onClick={() => saveState(user.id)}>
+              <div className="flex items-center space-x-2">
+                <UserImage />
+                  <span className="font-semibold text-blue-500">{`${user.first_name} ${user.last_name}`}</span>
+              </div>
+            </Link>
+            <div className="">
+              <TimeAgo timestamp={message.time_posted} />
+              { userId === message.senderid ?
+                <div className="space-x-1 flex justify-end mr-2">
+  
+                {/* <button onClick={() => onEdit()} className="text-red-600 cursor-pointer text-sm">Edit</button> */}
+                {/* <button onClick={() => onDeleteCommentClicked()} className="text-red-600 cursor-pointer text-sm">Delete</button> */}
+                </div>
+  
+                : ""
+              }
+            </div>
+  
+          </div>
+          {mode === SHOW && (          
+            <div>{message.text_body}</div>
+          )}
+          {/* {mode === EDITING && (
+            <EditCommentForm 
+              postId={postId}
+              commentId={comment.id}
+              onSaveEdit={onSaveEdit}
+              value={comment.text_body}
+            />
+          )} */}
+        </div>
+      </div>
+    );
+};
+
+export default UserMessageDetail;
