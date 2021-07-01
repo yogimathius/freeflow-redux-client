98457471aae5f713627638c4cfdff5db647d0d59
Mathius <devachievem@gmail.com>
2021-06-30 21:48:40 -0700

    adjust component import

--- src/pages/messages/index.js
+++ src/pages/messages/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import UserMessagesList from '../../features/messages/UserMessageList';
+import UserMessages from '../../features/messages/index';
 
 const index = () => {
     return (
         <div>
-            <UserMessagesList></UserMessagesList>
+            <UserMessages></UserMessages>
         </div>
     );
 };
