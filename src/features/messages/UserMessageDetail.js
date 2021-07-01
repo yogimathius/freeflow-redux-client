0748eb124dd7ae6e63c04ddd6758e203ea8c5deb
Mathius <devachievem@gmail.com>
2021-06-30 22:43:59 -0700

    aadjust message layout

--- src/features/messages/UserMessageDetail.js
+++ src/features/messages/UserMessageDetail.js
@@ -29,41 +29,54 @@ const UserMessageDetail = ({message, userId}) => {
         transition(SHOW);
     }
 
+    let fullname;
+    let placementStyle;
+
+    console.log(user.id, userId);
+    if (user.id === userId) {
+      fullname = 'You'
+      placementStyle = ''
+    } else {
+      fullname = user.first_name + ' ' + user.last_name
+      placementStyle = 'col-start-2'
+    }
     return (
-        <div key={message.id} className="bg-white  mx-1 border-2 border-solid border-green-500 border-opacity-25 my-2 rounded-xl">
-        <div className="p-3">
-          <div className="flex justify-between">
-            <Link to={`/userprofile/${user.id}`} onClick={() => saveState(user.id)}>
-              <div className="flex items-center space-x-2">
-                <UserImage />
-                  <span className="font-semibold text-blue-500">{`${user.first_name} ${user.last_name}`}</span>
-              </div>
-            </Link>
-            <div className="">
-              <TimeAgo timestamp={message.time_posted} />
-              { userId === message.senderid ?
-                <div className="space-x-1 flex justify-end mr-2">
-  
-                {/* <button onClick={() => onEdit()} className="text-red-600 cursor-pointer text-sm">Edit</button> */}
-                {/* <button onClick={() => onDeleteCommentClicked()} className="text-red-600 cursor-pointer text-sm">Delete</button> */}
+      <div className="grid grid-cols-3">
+          <div key={message.id} className={`${placementStyle} col-span-2 bg-white  mx-1 border-2 border-solid border-green-500 border-opacity-25 my-2 rounded-xl`}>
+          <div className=" p-3">
+            <div className="flex justify-between">
+              <Link to={`/userprofile/${user.id}`} onClick={() => saveState(user.id)}>
+                <div className="flex items-center space-x-2">
+                  <UserImage />
+                    <span className="font-semibold text-blue-500">{fullname}</span>
                 </div>
-  
-                : ""
-              }
+              </Link>
+              <div className="">
+                <TimeAgo timestamp={message.time_posted} />
+                { userId === message.senderid ?
+                  <div className="space-x-1 flex justify-end mr-2">
+    
+                  {/* <button onClick={() => onEdit()} className="text-red-600 cursor-pointer text-sm">Edit</button> */}
+                  {/* <button onClick={() => onDeleteCommentClicked()} className="text-red-600 cursor-pointer text-sm">Delete</button> */}
+                  </div>
+    
+                  : ""
+                }
+              </div>
+    
             </div>
-  
+            {mode === SHOW && (          
+              <div>{message.text_body}</div>
+            )}
+            {/* {mode === EDITING && (
+              <EditCommentForm 
+                postId={postId}
+                commentId={comment.id}
+                onSaveEdit={onSaveEdit}
+                value={comment.text_body}
+              />
+            )} */}
           </div>
-          {mode === SHOW && (          
-            <div>{message.text_body}</div>
-          )}
-          {/* {mode === EDITING && (
-            <EditCommentForm 
-              postId={postId}
-              commentId={comment.id}
-              onSaveEdit={onSaveEdit}
-              value={comment.text_body}
-            />
-          )} */}
         </div>
       </div>
     );
