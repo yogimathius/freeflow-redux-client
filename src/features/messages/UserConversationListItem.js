6f8eb096626c8a2e1d2a1736b0fe639c27630af2
Mathius <devachievem@gmail.com>
2021-06-30 23:29:32 -0700

    add router link to messagerName in conv list item

--- src/features/messages/UserConversationListItem.js
+++ src/features/messages/UserConversationListItem.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import {  useRouteMatch } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import { saveState } from '../../helpers/localStorage';
 
 
-const UserConversationListItem = ({messagerName}) => {
+const UserConversationListItem = ({messagerName, messagerId}) => {
     
 
     return (
-        <div className="text-black border-green-500 border-solid border-1 py-4 px-2 my-2 cursor-pointer">
-            {messagerName}
+        <div className="text-black border-green-500 rounded-xl bg-white border-solid border-1 py-4 px-2 my-2 ">
+            <Link to={`/userprofile/${messagerId}`} onClick={() => saveState(messagerId)} className="text-blue-500 font-semibold">
+                {messagerName}
+            </Link>
         </div>
     );
 };
