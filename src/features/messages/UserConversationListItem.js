954686d8ef529d97fe36deabaf4294eeffbefc3b
Mathius <devachievem@gmail.com>
2021-06-30 22:43:51 -0700

    dry

--- src/features/messages/UserConversationListItem.js
+++ src/features/messages/UserConversationListItem.js
@@ -1,15 +1,13 @@
 import React from 'react';
-import { Link, useRouteMatch } from 'react-router-dom'
+import {  useRouteMatch } from 'react-router-dom'
 
-import SelectedUserConversation from './SelectedUserConversation';
 
-const UserConversationListItem = ({conversation, messagerName}) => {
+const UserConversationListItem = ({messagerName}) => {
     
-    let { path, url } = useRouteMatch();
 
     return (
         <div className="text-black border-green-500 border-solid border-1 py-4 px-2 my-2 cursor-pointer">
-                {messagerName}
+            {messagerName}
         </div>
     );
 };
