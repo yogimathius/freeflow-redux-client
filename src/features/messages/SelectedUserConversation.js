acac93522a5351b4d4f621a38548087a285d4c76
Mathius <devachievem@gmail.com>
2021-06-30 22:12:35 -0700

    add userParams to selecteduserconvo

--- src/features/messages/SelectedUserConversation.js
+++ src/features/messages/SelectedUserConversation.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 const SelectedUserConversation = () => {
+    let { messagerId } = useParams();
+
+
     return (
         <div>
-            
+            From router: {messagerId}
         </div>
     );
 };
