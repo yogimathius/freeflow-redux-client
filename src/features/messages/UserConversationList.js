0b4692f1dc4d3f7346118867904421199a7a2864
Mathius <devachievem@gmail.com>
2021-06-30 22:12:48 -0700

    add router to conversation list

--- src/features/messages/UserConversationList.js
+++ src/features/messages/UserConversationList.js
@@ -2,20 +2,25 @@ import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import UserConversationListItem from './UserConversationListItem';
 import { fetchMessages, selectAllMessages, sortMessages } from './messagesSlice';
+import { Link, Route, Switch, useRouteMatch } from 'react-router-dom';
+import SelectedUserConversation from './SelectedUserConversation';
 // import SelectedUserConversation from './SelectedUserConversation';
 // import { onMessagerSelected } from './messageHelpers/onMessageSelected';
 
 const UserConversationList = () => {
     const loggedInUser = localStorage.getItem('user') ? JSON.parse(localStorage.getItem('user')) : ""
     const userId = loggedInUser.id;
+    let { path, url } = useRouteMatch();
+
     const userConversations = useSelector(selectAllMessages)
     
     const dispatch = useDispatch()
   
     let messageContent
-    // let selectedMessagesContent
+
     const messagesStatus = useSelector((state) => state.messages.status)
     const messagesError = useSelector((state) => state.messages.error)
+
     useEffect(() => {
       if (messagesStatus === 'idle') {
         dispatch(fetchMessages(userId))
@@ -28,17 +33,17 @@ const UserConversationList = () => {
     
       const sortedMessages = sortMessages(userConversations.messages, userId)
 
-      console.log('sorted in success: ', sortedMessages);
 
       let singleUserMessages;
 
       messageContent = sortedMessages.messagers.map((messagerName, index) => {
-        console.log(messagerName);
         const conversation = sortedMessages.messages[messagerName]
-        console.log(conversation);
+        console.log('conversation in list: ', conversation);
         return (
           <div key={index}>
-            <UserConversationListItem  conversation={conversation} messagerName={messagerName}/>
+            <Link to={`${url}/${messagerName}`}>
+              <UserConversationListItem  conversation={conversation} messagerName={messagerName}/>
+            </Link>
 
           </div>
         )
@@ -53,8 +58,16 @@ const UserConversationList = () => {
 
 
     return (
-        <div className="mt-10">
-            {messageContent}
+        <div className="mt-10 grid grid-cols-3">
+          <div>
+            {messageContent}  
+          </div>
+            <Switch>
+              <Route path={`${path}/:messagerId`}>
+                <SelectedUserConversation />
+              </Route>
+            </Switch>
+
         </div>
     );
 };
