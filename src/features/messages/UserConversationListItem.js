43b739bec3c1e82f1708433de7d984dfd9d3b718
Mathius <devachievem@gmail.com>
2021-07-04 21:16:33 -0700

    dry

--- src/features/messages/UserConversationListItem.js
+++ src/features/messages/UserConversationListItem.js
@@ -1,16 +1,13 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { saveState } from '../../helpers/localStorage';
-
 
 const UserConversationListItem = ({messagerName, messagerId}) => {
-    
+
 
     return (
-        <div className="text-black border-green-500 rounded-xl bg-white border-solid border-1 py-4 px-2 my-2 ">
-            <Link to={`/userprofile/${messagerId}`} onClick={() => saveState(messagerId)} className="text-blue-500 font-semibold">
+        <div className="text-black border-green-500 rounded-xl bg-white border-solid border-1 py-4 px-2 my-2">
+            <div className="text-blue-500 font-semibold z-50">
                 {messagerName}
-            </Link>
+            </div>
         </div>
     );
 };
