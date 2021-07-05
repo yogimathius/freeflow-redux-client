bf618ba3d1453c41e4f2b573b87edfee10f43471
Mathius <devachievem@gmail.com>
2021-07-04 22:25:55 -0700

    add text editor component

--- src/features/messages/SelectedUserConversation.js
+++ src/features/messages/SelectedUserConversation.js
@@ -1,21 +1,25 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
-import UserMessageDetail from './UserMessageDetail';
+/* eslint-disable react/prop-types */
+import React from 'react'
+import { useParams } from 'react-router-dom'
+import MessageTextEditor from './MessageTextEditor'
+import UserMessageDetail from './UserMessageDetail'
 
-const SelectedUserConversation = ({sortedMessages, userId}) => {
-    let { messagerId } = useParams();
-    const userMessages = sortedMessages.messages[messagerId]
+// eslint-disable-next-line react/prop-types
+const SelectedUserConversation = ({ sortedMessages, userId }) => {
+  const { messagerId } = useParams()
+  const userMessages = sortedMessages.messages[messagerId]
 
-    const renderedMessages = userMessages.map((message, index) => {
-        return (
-            <UserMessageDetail key={index} message={message} userId={userId}/>
-        )
-    })
+  const renderedMessages = userMessages.map((message, index) => {
     return (
+            <UserMessageDetail key={index} message={message} userId={userId}/>
+    )
+  })
+  return (
         <div className="col-span-2">
             {renderedMessages}
+            <MessageTextEditor messagerId={messagerId} />
         </div>
-    );
-};
+  )
+}
 
-export default SelectedUserConversation;
+export default SelectedUserConversation
