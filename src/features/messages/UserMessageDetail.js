15168673eb4783dae8deacd494c638cdc88dea98
Mathius <devachievem@gmail.com>
2021-07-04 22:26:18 -0700

    lint

--- src/features/messages/UserMessageDetail.js
+++ src/features/messages/UserMessageDetail.js
@@ -1,46 +1,45 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
-import { saveState } from '../../helpers/localStorage';
-import { TimeAgo } from '../posts/TimeAgo';
+/* eslint-disable react/prop-types */
+import React from 'react'
+import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
+import { saveState } from '../../helpers/localStorage'
+import { TimeAgo } from '../posts/TimeAgo'
 import useVisualMode from '../../hooks/useVisualMode'
-import UserImage from '../users/UserImage';
-import { selectUserById } from '../users/usersSlice';
+import UserImage from '../users/UserImage'
+import { selectUserById } from '../users/usersSlice'
 
-const SHOW = "SHOW";
+const SHOW = 'SHOW'
 // const CONFIRM = "CONFIRM";
 // const SAVING = "SAVING";
-const EDITING = "EDITING";
+// const EDITING = 'EDITING'
 // const ERROR_SAVE = "ERROR_SAVE";
 // const ERROR_DELETE = "ERROR_DELETE";
 
-const UserMessageDetail = ({message, userId}) => {
-    
-    const user = useSelector((state) => selectUserById(state, message.senderid))
+const UserMessageDetail = ({ message, userId }) => {
+  const user = useSelector((state) => selectUserById(state, message.senderid))
 
-    const { mode, transition } = useVisualMode(SHOW);
+  const { mode, transition } = useVisualMode(SHOW)
 
+  // function onEdit () {
+  //   transition(EDITING)
+  // }
 
-    function onEdit() {
-        transition(EDITING);
-    }
-    
-    function onSaveEdit() {
-        transition(SHOW);
-    }
+  // function onSaveEdit () {
+  //   transition(SHOW)
+  // }
 
-    let fullname;
-    let placementStyle;
+  let fullname
+  let placementStyle
 
-    if (user.id === userId) {
-      fullname = 'You'
-      placementStyle = ''
-    } else {
-      fullname = user.first_name + ' ' + user.last_name
-      placementStyle = 'col-start-2'
-    }
-    // const fullname = user.first_name + ' ' + user.last_name
-    return (
+  if (user.id === userId) {
+    fullname = 'You'
+    placementStyle = ''
+  } else {
+    fullname = user.first_name + ' ' + user.last_name
+    placementStyle = 'col-start-2'
+  }
+  // const fullname = user.first_name + ' ' + user.last_name
+  return (
       <div className="grid grid-cols-3">
           <div key={message.id} className={`${placementStyle} col-span-2 bg-white  mx-1 border-2 border-solid border-green-500 border-opacity-25 my-2 rounded-xl`}>
           <div className=" p-3">
@@ -53,23 +52,23 @@ const UserMessageDetail = ({message, userId}) => {
               </Link>
               <div className="">
                 <TimeAgo timestamp={message.time_posted} />
-                { userId === message.senderid ?
-                  <div className="space-x-1 flex justify-end mr-2">
-    
+                { userId === message.senderid
+                  ? <div className="space-x-1 flex justify-end mr-2">
+
                   {/* <button onClick={() => onEdit()} className="text-red-600 cursor-pointer text-sm">Edit</button> */}
                   {/* <button onClick={() => onDeleteCommentClicked()} className="text-red-600 cursor-pointer text-sm">Delete</button> */}
                   </div>
-    
-                  : ""
+
+                  : ''
                 }
               </div>
-    
+
             </div>
-            {mode === SHOW && (          
+            {mode === SHOW && (
               <div>{message.text_body}</div>
             )}
             {/* {mode === EDITING && (
-              <EditCommentForm 
+              <EditCommentForm
                 postId={postId}
                 commentId={comment.id}
                 onSaveEdit={onSaveEdit}
@@ -79,7 +78,7 @@ const UserMessageDetail = ({message, userId}) => {
           </div>
         </div>
       </div>
-    );
-};
+  )
+}
 
-export default UserMessageDetail;
+export default UserMessageDetail
