2c7183c5baf8cd27735fd3dbc7ea036cb4ec8a92
Mathius <devachievem@gmail.com>
2021-07-11 23:13:40 -0700

    adjust styles in user conv list item

--- src/features/messages/UserConversationListItem.js
+++ src/features/messages/UserConversationListItem.js
@@ -4,11 +4,11 @@ import { faChevronRight } from '@fortawesome/free-solid-svg-icons'
 
 const UserConversationListItem = ({ messagerName, messagerId }) => {
   return (
-        <div className="transition duration-500 ease-in-out  hover:bg-red-600 transform hover:translate-x-1 hover:scale-100 flex justify-between items-center text-black border-green-500 rounded-xl bg-white border-solid border-1 py-6 px-6 my-2">
-            <div className="text-blue-500 font-semibold z-50">
+        <div className="group transition duration-500 ease-in-out  hover:bg-green-500 hover:text-white transform hover:translate-x-1 hover:scale-100 flex justify-between items-center text-black rounded-xl bg-white py-6 px-6 my-2">
+            <div className="text-green-500 group-hover:text-white font-semibold z-50">
                 {messagerName}
             </div>
-            <FontAwesomeIcon className="text-blue-500" icon={faChevronRight} />
+            <FontAwesomeIcon className="text-green-500 group-hover:text-white " icon={faChevronRight} />
         </div>
   )
 }
