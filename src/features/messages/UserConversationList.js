e861a88ae3b9a47c12ae20f73a20463073ba4571
Mathius <devachievem@gmail.com>
2021-07-13 23:23:49 -0700

    add user selector

--- src/features/messages/UserConversationList.js
+++ src/features/messages/UserConversationList.js
@@ -1,12 +1,34 @@
+/* eslint-disable no-unused-vars */
 import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import UserConversationListItem from './messagerNameItem'
-import { fetchMessages, selectAllMessages, sortMessages } from './messagesSlice'
 import { Link, Route, Switch, useRouteMatch } from 'react-router-dom'
+
+import { fetchMessages, selectAllMessages, sortMessages } from './messagesSlice'
+import useVisualMode from '../../hooks/useVisualMode'
+
 import SelectedUserConversation from './SelectedUserConversation'
+import UserConversationListItem from './messagerNameItem'
+import CreateMessageForm from './CreateMessageForm'
+import UsernameSelector from './UsernameSelector'
+
+const SHOW = 'SHOW'
+// const CONFIRM = "CONFIRM";
+// const SAVING = "SAVING";
+const COMPOSE = 'COMPOSE'
+// const ERROR_SAVE = "ERROR_SAVE";
+// const ERROR_DELETE = "ERROR_DELETE";
 
 const UserConversationList = () => {
   const [currentPage, setCurrentPage] = useState('')
+  const { mode, transition } = useVisualMode(SHOW)
+
+  function onComposeMessage () {
+    transition(COMPOSE)
+  }
+
+  function onSendComposeMessage () {
+    transition(SHOW)
+  }
 
   const loggedInUser = localStorage.getItem('user') ? JSON.parse(localStorage.getItem('user')) : ''
   const userId = loggedInUser.id
@@ -44,7 +66,6 @@ const UserConversationList = () => {
                 />
 
             </Link>
-
           </div>
       )
     })
@@ -54,23 +75,48 @@ const UserConversationList = () => {
 
   return (
       <div className="grid grid-cols-4 grid-rows-8 mx-4">
-          <div className="row-start-1 col-span-3 mb-8 mr-4">
-            <button className="btn btn-primary ">
-              Compose Message
-            </button>
-          </div>
+          {/* {mode === SHOW && (
+
+            <div className="row-start-1 flex justify-center my-8 ml-4">
+              <button className="btn btn-primary " onClick={onComposeMessage}>
+                Compose
+              </button>
+            </div>
+          )} */}
+          {/* {mode === COMPOSE && ( */}
+            <div className="col-span-4 grid grid-cols-4 items-center my-7">
+              {/* <div className="flex justify-center"> */}
+              {/* <button onClick={() => transition(SHOW)} className="btn btn-warning col-span-1">Cancel</button> */}
+              {/* </div> */}
+              <UsernameSelector
+                sortedMessages={sortedMessages}
+                userId={userId}
+                messagers={sortedMessages?.messagers}
+                currentPage={currentPage}
+                setCurrentPage={setCurrentPage}
+                url={url}
+                path={path}
+              />
+            </div>
+          {/* )} */}
           <div className="row-start-2 col-span-1 col-start-1 row-span-6 flex flex-col">
             {renderedMessagers}
           </div>
-          <div className="row-start-3 row-span-7 col-start-2 col-span-3 ">
-            <Switch>
-              <Route path={`${path}/:messagerId`}>
-                <SelectedUserConversation
-                  sortedMessages={sortedMessages} userId={userId}
-                />
-              </Route>
-            </Switch>
-          </div>
+          {mode === SHOW && (
+            <div className="row-start-3 row-span-7 col-start-2 col-span-3 ">
+              <Switch>
+                <Route path={`${path}/:messagerId`}>
+                  <SelectedUserConversation
+                    sortedMessages={sortedMessages} userId={userId}
+                  />
+                </Route>
+              </Switch>
+            </div>
+          )}
+
+          {mode === COMPOSE && (
+            <CreateMessageForm />
+          )}
 
         </div>
   )
