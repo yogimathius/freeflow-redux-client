7bd4b3fa330e77a07e635d62864ad737dcf3a39b
Mathius <devachievem@gmail.com>
2021-07-14 23:29:03 -0700

    add user image back

--- src/features/users/UserNameAndLogo.js
+++ src/features/users/UserNameAndLogo.js
@@ -10,7 +10,7 @@ export const UserNameAndLogo = ({ userId }) => {
   function getRandomInt () {
     return Math.floor(Math.random() * (10000 - 5)) + 4
   }
-  // const imgUrl = "http://graph.facebook.com/v2.5/" + getRandomInt() + "/picture";
+  const imgUrl = 'http://graph.facebook.com/v2.5/' + getRandomInt() + '/picture'
 
   const fullName = author ? `${author.first_name} ${author.last_name}` : ''
   const isActive = author && author.active ? <p className="text-green-400 text-sm font-semibold">Online</p> : <p className="text-gray-500 text-sm font-semibold">Offline</p>
@@ -18,7 +18,7 @@ export const UserNameAndLogo = ({ userId }) => {
   return (
     <div className="flex items-center space-x-2 w-max ml-2">
       <div className="w-10 md:w-full">
-        {/* <img className="inline-block rounded-full p-2 border-2 border-solid border-green-500" alt="avatar" src={imgUrl} /> */}
+        <img className="inline-block rounded-full p-2 border-2 border-solid border-green-500" alt="avatar" src={imgUrl} />
       </div>
 
       <div className="inline-block">
