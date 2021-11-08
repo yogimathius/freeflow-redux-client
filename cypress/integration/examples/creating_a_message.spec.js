cb809b4625fbe2d8c04f61b49d984a8bdd4114bc
Mathius <devachievem@gmail.com>
2021-11-07 20:28:33 -0800

    add cypress test for create a message

--- cypress/integration/examples/creating_a_message.spec.js
+++ cypress/integration/examples/creating_a_message.spec.js
@@ -1,29 +1,27 @@
 describe('Creating a post', () => {
-
   it('Displays the post in the list', () => {
-    cy.visit('/');
+    cy.visit('/')
 
-		cy.get('[data-testid="postTitle"]')
-		.type('This is a New title');
-		
-		cy.get('select')
-			.select('Test FreeFlow')
+    cy.get('[data-testid="postTitle"]')
+      .type('This is a New title')
+
+    cy.get('select')
+      .select('Test FreeFlow')
 
     cy.get('[data-testid="postText"]')
-      .type('This is a New post');
+      .type('This is a New post')
 
     cy.get('[data-testid="sendButton"]')
-      .click({force: true});
+      .click({ force: true })
 
     cy.get('[data-testid="postText"]')
 
-		cy.contains('This is a New post');
-		
-		cy.get('.post-excerpt > p')
-		
-		cy.contains('This is a New title');
+    cy.contains('This is a New post')
 
-		cy.contains('This is a New post');
+    cy.get('.post-excerpt > p')
 
-  });
-});
+    cy.contains('This is a New title')
+
+    cy.contains('This is a New post')
+  })
+})
