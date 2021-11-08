60d17bc6de08b6b7bda1df9ce4a6f8cf56efb6f7
Mathius <devachievem@gmail.com>
2021-11-07 20:32:13 -0800

    create unread count slice

---
+++ src/reducers/unreadCountSlice.js
@@ -0,0 +1,60 @@
+import {
+  createSlice,
+  createAsyncThunk,
+  createEntityAdapter
+  // createSelector,
+} from '@reduxjs/toolkit'
+import axios from 'axios'
+
+const url = 'https://freeflow-two-point-o.herokuapp.com/api/messages'
+
+export const fetchUnreadCount = createAsyncThunk('messages/fetchMessages', async (userId) => {
+  const response = await axios.get(`${url}/unread_count`, { userID: userId })
+  return response.data
+})
+
+const initialState = {
+  status: 'idle',
+  error: null
+}
+
+const unreadCountSlice = createSlice({
+  name: 'unreadCount',
+  initialState,
+  reducers: {
+    // messageUpdated (state, action) {
+    //   const { id, content } = action.payload
+    //   const existingPost = state.entities[id]
+    //   if (existingPost) {
+    //     existingPost.content = content
+    //   }
+    // }
+  },
+  extraReducers: {
+    [fetchUnreadCount.pending]: (state, action) => {
+      state.status = 'loading'
+    },
+    [fetchUnreadCount.fulfilled]: (state, action) => {
+      state.status = 'succeeded'
+      state.unreadCount = action.payload
+    },
+    [fetchUnreadCount.rejected]: (state, action) => {
+      state.status = 'failed'
+      state.error = action.error.message
+    }
+
+    // [removeMessage.fulfilled]: (state, action) => {
+    //   unreadCountAdapter.removeOne(state, action.meta.arg.messageId)
+    // }
+    // [updateMessage.fulfilled]:(state, action) => {
+    //   // const { id, ...changes } = payload;
+    //   unreadCountAdapter.upsertOne(state, action.meta.arg)
+    // }
+  }
+})
+
+// export const { messageAdded, messageUpdated } = unreadCountSlice.actions
+
+export default unreadCountSlice.reducer
+
+export const selectAllMessages = state => state.unreadCount
