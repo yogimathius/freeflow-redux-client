6042b6fe800376a25383db8e70eab8f51776bbb9
Mathius <devachievem@gmail.com>
2022-02-05 22:58:57 -0800

    load state in app route

--- src/components/AppRoute.js
+++ src/components/AppRoute.js
@@ -1,15 +1,16 @@
 /* eslint-disable no-mixed-spaces-and-tabs */
 /* eslint-disable no-tabs */
-import React from 'react'
+import React, { useState } from 'react'
 import { useSelector } from 'react-redux'
 import { Redirect, Route } from 'react-router-dom'
+import { loadState } from '../helpers/localStorage'
 
 const AppRoutes = ({ component: Component, path, isPrivate, props, ...rest }) => {
-  const loggedInUser = useSelector(state => state.user.user)
+  const loggedInUserID = loadState()
   return (
 		<Route
 			path={path}
-			render={(props) => isPrivate && loggedInUser === undefined
+			render={(props) => isPrivate && loggedInUserID === undefined
 			  ? (<Redirect to={{ pathname: '/login' }} />)
 			  : (<Component {...props} />)
 			}
