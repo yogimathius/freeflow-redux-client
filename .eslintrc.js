632ac8518303eb1b3f29b689639f68921ef0869c
Mathius <devachievem@gmail.com>
2022-02-09 20:39:19 -0800

    create eslint file

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,8 @@ module.exports = {
   },
   extends: [
     'plugin:react/recommended',
-    'standard'
+    'standard',
+    'plugin:cypress/recommended'
   ],
   parserOptions: {
     ecmaFeatures: {
