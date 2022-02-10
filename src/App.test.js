67077ed7009880e5f2f50fe596c38557a1756dc2
Mathius <mathiusjohnsondeveloper@gmail.com>
2022-02-09 21:28:53 -0800

    refactor app test file

--- src/App.test.js
+++ src/App.test.js
@@ -1,48 +1,20 @@
-/**
- * @jest-environment jsdom
- */
-
 import React from 'react'
 import { rest } from 'msw'
 import { setupServer } from 'msw/node'
 import 'regenerator-runtime/runtime'
-import { CSSTransition } from 'react-transition-group'
 
 // We're using our own custom render function and not RTL's render.
 // Our custom utils also re-export everything from RTL
 // so we can import fireEvent and screen here as well
-import { render, fireEvent, screen } from './features/test-utils'
+import { render } from './features/test-utils'
 import App from './App'
 
 // We use msw to intercept the network request during the test,
 // and return the response 'John Smith' after 150ms
 // when receiving a get request to the `/api/user` endpoint
 export const handlers = [
-  // rest.get('/api/user', (req, res, ctx) => {
-  //   return res(ctx.json('John Smith'), ctx.delay(150))
-  // }),
   rest.get('https://freeflow-two-point-o.herokuapp.com/api/user_skills', null)
 ]
-
-function Fade ({ children, ...props }) {
-  return (
-    <CSSTransition {...props} timeout={1000} classNames="fade">
-      {children}
-    </CSSTransition>
-  )
-}
-
-jest.mock('react-transition-group', () => {
-  const FakeCSSTransition = jest.fn(() => null)
-  return { CSSTransition: FakeCSSTransition }
-})
-
-jest.mock('@material-ui/core/styles', () => ({
-  withStyles: styles => component => component
-}))
-/**
- * @jest-environment jsdom
- */
 
 jest.mock('@material-ui/icons', () => {
   const icons = {
@@ -58,23 +30,10 @@ jest.mock('@material-ui/icons', () => {
   return new Proxy(icons, handler)
 })
 
-jest.mock('@material-ui/core/Menu')
-jest.mock('@material-ui/core/MenuItem')
-jest.mock('@material-ui/core/ListItemIcon')
-jest.mock('@material-ui/core/ListItemText')
-jest.mock('@material-ui/core/Button')
-
 const server = setupServer(...handlers)
 
 // Enable API mocking before tests.
 beforeAll(() => {
-  const localStorage = {
-    getItem: jest.fn(),
-    setItem: jest.fn(),
-    clear: jest.fn()
-  }
-  globalThis.localStorage = localStorage
-
   server.listen()
 })
 
