f8e37a4579ce629a7aca36a5db7b2aeaea89dd22
Mathius <devachievem@gmail.com>
2022-02-09 20:39:38 -0800

    add jest config

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  verbose: true,
+  setupFilesAfterEnv: ['<rootDir>src/setupTests.js'],
+  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
+  moduleDirectories: ['node_modules', 'src'],
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/fileMock.js',
+    '\\.(css|less)$': '<rootDir>/__mocks__/styleMock.js'
+
+  },
+  transform: {
+    '^.+\\.(js|jsx)$': 'babel-jest',
+    '.+\\.(css|scss|png|jpg|svg)$': 'jest-transform-stub',
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+    '<rootDir>/__mocks__/fileTransformer.js'
+  },
+  testEnvironment: 'jsdom'
+
+}
