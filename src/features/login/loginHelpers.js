a235a2ad17e4d1d5f4c7545d4d80b089a62d7a12
Mathius <mathiusjohnsondeveloper@gmail.com>
2022-02-25 14:09:54 -0800

    add more helpers

---
+++ src/features/login/loginHelpers.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { fetchUserSkills } from '../../reducers/userSkillsSlice'
+import { fetchSkills } from '../../reducers/dbSkillsSlice'
+import { login } from '../../reducers/userLoginSlice'
+import { Redirect } from 'react-router-dom'
+import { loadState } from '../../helpers/localStorage'
+
+export const onLoginSubmitted = async (username, password, dispatch) => {
+  console.log('login submitted')
+  try {
+    dispatch(login(username, password))
+  } catch (err) {
+    console.error('Failed to login user: ', err)
+  } finally {
+    dispatch(fetchSkills())
+    dispatch(fetchUserSkills())
+  }
+}
+
+export const checkLoggedIn = (dispatch) => {
+  const user = loadState()
+
+  if (user !== undefined) {
+    dispatch(fetchSkills())
+    dispatch(fetchUserSkills())
+
+    return (
+      <Redirect to={{ pathname: '/dashboard' }} />
+    )
+  }
+}
