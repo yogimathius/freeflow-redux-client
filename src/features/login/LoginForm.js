00ed6a776e446c81000338b190fc44fdd3acb0a7
Mathius <mathiusjohnsondeveloper@gmail.com>
2022-02-25 14:09:32 -0800

    add LoginForm component

---
+++ src/features/login/LoginForm.js
@@ -0,0 +1,65 @@
+import { ErrorMessage, Field, Form, Formik } from 'formik'
+import React from 'react'
+
+const LoginForm = ({ onLoginSubmitted, dispatch }) => {
+  return (
+    <Formik
+    className="flex justify-center"
+    initialValues={{ username: '', password: '' }}
+    onSubmit={(values) => {
+      const username = values.username
+      const password = values.password
+      onLoginSubmitted(username, password, dispatch)
+    }}
+    validate={(values) => {
+      const errors = {}
+      if (!values.username) {
+        errors.username = '*Required*'
+      }
+      if (!values.password) {
+        errors.password = '*Required*'
+      }
+
+      return errors
+    }}
+  >
+      <Form className="flex flex-col w-2/3 md:w-1/4 mx-auto space-y-3">
+        <label htmlFor="username">username</label>
+        <Field
+          id="username"
+          data-testid="username"
+          className="text-center rounded-xl py-1 border-1 border-gray-400"
+          placeholder="Username"
+          type="text"
+          name="username" />
+          <div className='text-red-500'>
+            <ErrorMessage data-testid="usernameError" name="username" component="div" />
+          </div>
+
+        <label htmlFor="password">password</label>
+        <Field
+          id="password"
+          data-testid="password"
+          className="text-center rounded-xl py-1 border-1 border-gray-400"
+          placeholder="Password"
+          type="password"
+          name="password" />
+          <div className='text-red-500'>
+            <ErrorMessage data-testid="passwordError" name="password" component="div" />
+          </div>
+
+        <div className="flex justify-center">
+          <button
+            role='button'
+            data-testid='login-button'
+            className="btn btn-primary"
+            type="submit" >
+              Login
+          </button>
+        </div>
+      </Form>
+  </Formik>
+  )
+}
+
+export default LoginForm
