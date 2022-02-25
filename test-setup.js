00a1e5dc51ee1a10518d18debcd25ddb5a90be9a
Mathius <mathiusjohnsondeveloper@gmail.com>
2022-02-25 14:50:42 -0800

    extend matchers

--- test-setup.js
+++ test-setup.js
@@ -1,5 +1,7 @@
 // setup file
 import { configure } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
+import * as matchers from 'jest-extended'
 
 configure({ adapter: new Adapter() })
+expect.extend(matchers)
