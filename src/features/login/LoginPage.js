aa78bc9b64baf38a58dd9cf587f43a6d78d4e81a
Mathius <mathiusjohnsondeveloper@gmail.com>
2022-02-25 15:38:23 -0800

    refactor login page

--- src/features/login/LoginPage.js
+++ src/features/login/LoginPage.js
@@ -1,23 +1,23 @@
 import React from 'react'
 import { useDispatch } from 'react-redux'
 import { Redirect } from 'react-router-dom'
-import { fetchSkills } from '../../reducers/dbSkillsSlice'
-import { fetchUserSkills } from '../../reducers/userSkillsSlice'
-
-import { loadState } from '../../helpers/localStorage'
 import LoginForm from './LoginForm'
-import { checkLoggedIn, onLoginSubmitted } from './loginHelpers'
+import { checkLoggedIn, handleLogin } from './loginHelpers'
 import LoginDetails from './LoginDetails'
 
 export default function LoginPage () {
   const dispatch = useDispatch()
 
-  checkLoggedIn(dispatch)
+  if (checkLoggedIn(dispatch)) {
+    return (
+      <Redirect to={{ pathname: '/dashboard' }} />
+    )
+  }
 
   return (
     <div className="text-center space-y-3 bg-white flex flex-col justify-center items-center h-96">
       <h1 className="text-2xl">Please login to view content.</h1>
-      <LoginForm onLoginSubmitted={onLoginSubmitted} dispatch={dispatch}/>
+      <LoginForm handleLogin={handleLogin} />
 
       <LoginDetails />
     </div>
