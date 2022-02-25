4c26d650df9fdc6cfe9ef164708d859e43eee216
Mathius <mathiusjohnsondeveloper@gmail.com>
2022-02-25 14:09:24 -0800

    add LoginDetails component

--- src/features/login/LoginDetails.js
+++ src/features/login/LoginDetails.js
@@ -3,9 +3,9 @@ import React from 'react'
 const LoginDetails = () => {
   return (
     <div>
-    <div>You can use these login details to test the app:</div>
-    <div>Username: <span className="font-bold">dsleaford1</span></div>
-    <div>Password: <span className="font-bold">4lGhIyW</span></div>
+      <div>You can use these login details to test the app:</div>
+      <div>Username: <span className="font-bold">dsleaford1</span></div>
+      <div>Password: <span className="font-bold">4lGhIyW</span></div>
   </div>
   )
 }
