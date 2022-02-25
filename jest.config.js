65b3c066c3fcb8f6b46dcf91a66fa208d0f42de1
Mathius <mathiusjohnsondeveloper@gmail.com>
2022-02-25 13:46:49 -0800

    adjust jest config

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   verbose: true,
-  setupFilesAfterEnv: ['<rootDir>src/setupTests.js'],
+  setupFilesAfterEnv: ['<rootDir>src/setupTests.js', 'jest-extended/all', '<rootDir>__tests__/setup/setupEnzyme.js'],
+  testPathIgnorePatterns: ['<rootDir>/__tests__/setup/*'],
   moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
   moduleDirectories: ['node_modules', 'src'],
   moduleNameMapper: {
@@ -14,6 +15,9 @@ module.exports = {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
     '<rootDir>/__mocks__/fileTransformer.js'
   },
-  testEnvironment: 'jsdom'
-
+  testEnvironment: 'jsdom',
+  collectCoverageFrom: [
+    'src/**/*.js',
+    '!src/index.js'
+  ]
 }
