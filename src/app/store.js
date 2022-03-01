c8b3f5f222225941635205b496659e56c44fbc07
Mathius <mathiusjohnsondeveloper@gmail.com>
2022-03-01 10:33:57 -0800

    add middleware warning timers to configurestore

--- src/app/store.js
+++ src/app/store.js
@@ -2,7 +2,11 @@ import { configureStore } from '@reduxjs/toolkit'
 import rootReducer from '../reducers/index'
 
 const store = configureStore({
-  reducer: rootReducer
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+    immutableCheck: { warnAfter: 128 },
+    serializableCheck: { warnAfter: 128 }
+  })
 })
 
 export default store
