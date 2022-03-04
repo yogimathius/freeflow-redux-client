792bde3fb6d0ea801363a9b32281d4783d3793df
Mathius <mathiusjohnsondeveloper@gmail.com>
2022-03-04 13:24:46 -0800

    move editpostform

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,7 +1,7 @@
 import { VisiblePostsList } from '../features/posts/VisiblePostsList'
 import { UsersList } from '../features/users/UsersList'
 import { SinglePostPage } from '../features/posts/SinglePostPage'
-import { EditPostForm } from '../features/posts/EditPostForm'
+import { EditPostForm } from '../features/posts/components/EditPostForm/EditPostForm'
 import UserPage from '../features/users/UserPage'
 import LoginPage from '../features/login/LoginPage'
 import UserExperiences from '../features/experiences/UserExperiences'
