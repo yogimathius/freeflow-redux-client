f0bd667a711998ac975231f74ff73ba2c74ca058
Mathius <mathiusjohnsondeveloper@gmail.com>
2022-03-04 14:31:15 -0800

    create commentsCount component

---
+++ src/features/posts/components/CommentsCount/CommentsCount.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const CommentsCount = ({ commentsLength, setShowMessage, showMessage }) => {
+  const commentCount = commentsLength > 1 ? commentsLength + ' comments' : commentsLength + 'comment'
+
+  return (
+    <button type="button" onClick={() => setShowMessage(!showMessage)} className="">
+      <span>{commentCount}</span>
+    </button>
+  )
+}
+
+export default CommentsCount
---
+++ src/features/posts/components/CommentsCount/index.js
@@ -0,0 +1,1 @@
+export { CommentsCount } from './CommentsCount'
