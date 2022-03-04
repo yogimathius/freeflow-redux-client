3021c71568edcb73cc46740a0668ab92c2342b19
Mathius <mathiusjohnsondeveloper@gmail.com>
2022-03-04 13:26:27 -0800

    create SaveEditButton component

---
+++ src/features/posts/components/EditPostForm/components/SaveEditButton.js
@@ -0,0 +1,39 @@
+import React from 'react'
+
+const SaveEditButton = ({
+  validPost,
+  content,
+  onEditPostClicked,
+  setAddRequestStatus,
+  dispatch,
+  updatePost,
+  unwrapResult,
+  onSaveEdit,
+  post
+}) => {
+  return (
+    <div className="flex justify-center">
+    <button
+      className="btn btn-tertiary"
+      type="button"
+      onClick={() => {
+        if (validPost()) {
+          onEditPostClicked(
+            content,
+            setAddRequestStatus,
+            dispatch,
+            updatePost,
+            unwrapResult,
+            onSaveEdit,
+            post
+          )
+        }
+      }}
+      data-testid="saveButton">
+      Save Post
+    </button>
+  </div>
+  )
+}
+
+export default SaveEditButton
