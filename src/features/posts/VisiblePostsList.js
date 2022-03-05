33da5e669e566dd470384a00292a92ff044d0907
Mathius <mathiusjohnsondeveloper@gmail.com>
2022-03-04 22:11:59 -0800

    refactor VisiblePostsList

--- src/features/posts/VisiblePostsList.js
+++ src/features/posts/VisiblePostsList.js
@@ -2,6 +2,7 @@ import { connect } from 'react-redux'
 import { createSelector } from '@reduxjs/toolkit'
 import PostsList from './PostsList'
 import { VisibilityFilters } from '../../reducers/filtersSlice'
+import { extractSinglePosts } from './utils/extractSinglePosts'
 
 const selectPosts = (state) => state.posts
 const selectSkills = (state) => state.skills
@@ -10,24 +11,7 @@ const selectFilter = (state) => state.visibilityFilters
 const selectVisiblePosts = createSelector(
   [selectPosts, selectSkills, selectFilter],
   (posts, skills, filter) => {
-    const postKeys = Object.keys(posts.entities)
-    const skillKeys = Object.keys(skills.entities)
-
-    const postSkillsArr = []
-
-    const postArr = []
-    postKeys.forEach(postKey => {
-      const singlePosts = JSON.parse(JSON.stringify(posts.entities[postKey]))
-
-      singlePosts.skills = []
-      skillKeys.forEach(skillKey => {
-        postSkillsArr.push(skills.entities[skillKey])
-        if (singlePosts.skill_ids.includes(skills.entities[skillKey].id)) {
-          singlePosts.skills.push(skills.entities[skillKey].name)
-        }
-      })
-      postArr.push(singlePosts)
-    })
+    const { postArr } = extractSinglePosts(posts, skills)
     const sortedArr = [...postArr].sort((a, b) => new Date(b.time_posted) - new Date(a.time_posted))
     for (const skill in VisibilityFilters) {
       const filteredSkill = VisibilityFilters[skill]
