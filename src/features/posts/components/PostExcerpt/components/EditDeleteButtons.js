c69f6cdc1b7c59c942b157b430e9a3607101aa3b
Mathius <mathiusjohnsondeveloper@gmail.com>
2022-03-06 22:25:31 -0800

    Create EditDeleteButtons component

--- src/features/posts/components/PostExcerpt/components/EditDeleteButtons.js
+++ src/features/posts/components/PostExcerpt/components/EditDeleteButtons.js
@@ -3,22 +3,22 @@ import React from 'react'
 const EditDeleteButtons = ({ onEdit, onConfirmDelete, transition, EDITING, CONFIRM }) => {
   return (
     <div className="space-x-1 flex justify-end mr-2">
-    <button
-      role="button"
-      onClick={() => onEdit(transition, EDITING) }
-      className="text-red-600 cursor-pointer text-sm"
-      data-testid="editButton"
-    >
-        Edit
-    </button>
-    <button
-      onClick={() => onConfirmDelete(transition, CONFIRM)}
-      className="text-red-600 cursor-pointer text-sm"
-      data-testid="confirmDeleteButton"
-    >
-      Delete
-    </button>
-  </div>
+      <button
+        role="button"
+        onClick={() => onEdit(transition, EDITING) }
+        className="text-red-600 cursor-pointer text-sm"
+        data-testid="editButton"
+      >
+          Edit
+      </button>
+      <button
+        onClick={() => onConfirmDelete(transition, CONFIRM)}
+        className="text-red-600 cursor-pointer text-sm"
+        data-testid="confirmDeleteButton"
+      >
+        Delete
+      </button>
+    </div>
   )
 }
 
