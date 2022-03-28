7fef460097bfd4ce6ba745b5271f70b68e4b1e04
Mathius <mathiusjohnsondeveloper@gmail.com>
2022-03-27 21:51:54 -0700

    fix view comment text styles

--- src/features/posts/components/CommentsCount/CommentsCount.js
+++ src/features/posts/components/CommentsCount/CommentsCount.js
@@ -5,7 +5,7 @@ const CommentsCount = ({ commentsLength, setShowMessage, showMessage }) => {
 
   const view = showMessage ? 'Hide ' : 'View '
   return (
-    <button type="button" onClick={() => setShowMessage(!showMessage)} className="flex justify-center w-full">
+    <button type="button" onClick={() => setShowMessage(!showMessage)} className="flex justify-center w-full font-semibold text-gray-500 underline">
       <span>{view} {commentCount}</span>
     </button>
   )
