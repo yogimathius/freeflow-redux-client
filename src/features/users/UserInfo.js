e2aeb72b6a7fb87035821f34c70bd57a0e2a1e84
Mathius <mathiusjohnsondeveloper@gmail.com>
2022-03-30 22:35:51 -0700

    refactor userinfo

--- src/features/users/UserInfo.js
+++ src/features/users/UserInfo.js
@@ -1,13 +1,11 @@
-/* eslint-disable react/no-unescaped-entities */
-/* eslint-disable react/prop-types */
 import React from 'react'
 
 const UserInfo = ({ profession, tagline, location, position }) => {
   return (
-    <div className={`flex flex-col justify-between space-y-1 md:${position}`}>
+    <div className={`flex flex-col justify-center space-y-1 md:${position}`}>
       <div className="md:text-lg font-bold">{profession}</div>
-      <div className="hidden md:block md:text-base italic">"{tagline}"</div>
-      <div className="text-sm md:text-base font-semibold text-green-500">{location}</div>
+      <div className="hidden md:block md:text-base italic">{`"${tagline}"`}</div>
+      {/* <div className="text-sm font-semibold">From: {location}</div> */}
     </div>
   )
 }
