4733793cae3d37caee96fb9364bb4e4060d156bf
Mathius <mathiusjohnsondeveloper@gmail.com>
2022-03-30 22:36:03 -0700

    refactor usercard

--- src/features/users/UserCard.js
+++ src/features/users/UserCard.js
@@ -16,29 +16,26 @@ export default function UserCard ({ user, infoPosition }) {
   const position = infoPosition === 'right' ? 'text-right' : 'text-left'
 
   return (
-    <div className='flex flex-col justify-start md:justify-between bg-white rounded-xl my-1 hover:shadow-lg space-y-4 p-3 h-full' key={user.id}>
-      <div className="flex flex-col mr-4 space-y-2">
-        <div className="flex flex-col md:flex-row justify-between space-y-2 md:space-y-0">
-          <div className="space-y-6">
-            <Link to={`/userprofile/${user.id}`} onClick={() => saveState(user.id)}>
-              <UserNameAndLogo userId={user.id} />
-            </Link>
-            <ProgressBar experience={experience} />
-          </div>
-          <UserInfo
-            profession={user.profession}
-            tagline={user.tagline}
-            location={user.location}
-            position={position}
-          />
+    <div className='flex flex-col justify-start md:justify-between bg-white rounded-xl hover:shadow-lg space-y-4 p-4 h-full' key={user.id}>
+      <div className="flex flex-col md:flex-row justify-between space-y-2 md:space-y-0">
+        <div className="space-y-4">
+          <Link to={`/userprofile/${user.id}`} onClick={() => saveState(user.id)}>
+            <UserNameAndLogo userId={user.id} />
+          </Link>
+          <ProgressBar experience={experience} />
         </div>
+        <UserInfo
+          profession={user.profession}
+          tagline={user.tagline}
+          location={user.location}
+          position={position}
+        />
       </div>
       <div className="text-sm">
         <UserSkills
           userId={user.id}
         />
       </div>
-
     </div>
   )
 }
