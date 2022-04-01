4d420c6ba506b1a39fc63c47151d51815fa31876
Mathius <mathiusjohnsondeveloper@gmail.com>
2022-03-31 21:05:29 -0700

    pass isSideBar to UserNameAndLogo

--- src/features/users/UserNameAndLogo.js
+++ src/features/users/UserNameAndLogo.js
@@ -4,8 +4,9 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 
 import { selectUserById } from '../../reducers/usersSlice'
+import IsOnline from './components/IsOnline'
 
-export const UserNameAndLogo = ({ userId }) => {
+export const UserNameAndLogo = ({ userId, isSideBar }) => {
   const author = useSelector((state) => selectUserById(state, userId))
   function getRandomInt () {
     return Math.floor(Math.random() * (10000 - 5)) + 4
@@ -13,17 +14,19 @@ export const UserNameAndLogo = ({ userId }) => {
   const imgUrl = 'http://graph.facebook.com/v2.5/' + getRandomInt() + '/picture'
 
   const fullName = author ? `${author.first_name} ${author.last_name}` : ''
-  const isActive = author && author.active ? <p className="text-green-400 text-sm font-semibold">Online</p> : <p className="text-gray-500 text-sm font-semibold">Offline</p>
 
+  const isSideBarFont = isSideBar ? '' : 'md:text-lg'
+
+  const isSideBarLogo = isSideBar ? 'w-1/4' : 'w-max'
   return (
     <div className="flex space-x-1 items-center">
-      <div className="w-max mr-4">
+      <div className={`${isSideBarLogo} mr-4`}>
         <img className="inline-block rounded-full p-2 border-2 border-solid border-green-500" alt="avatar" src={imgUrl} />
       </div>
 
       <div className="w-max">
-        <p className="inline-block text-sm md:text-lg font-bold text-blue-500">{author ? fullName : 'Unknown author'}</p>
-        {isActive}
+        <p className={`inline-block text-sm ${isSideBarFont} font-bold text-blue-500`}>{author ? fullName : 'Unknown author'}</p>
+        <IsOnline author={author} />
       </div>
     </div>
   )
