a61e041ec36bd3c735522986eb0f3ae9ff4d39c7
Mathius <mathiusjohnsondeveloper@gmail.com>
2022-04-03 20:10:13 -0700

    create Posts page

---
+++ src/pages/posts/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { VisiblePostsList } from '../../features/posts'
+
+const index = () => {
+  return (
+    <div>
+      <VisiblePostsList></VisiblePostsList>
+    </div>
+  )
+}
+
+export default index
