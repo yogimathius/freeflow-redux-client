c713c6bb60f3674b438cb9d34a586075fa69349e
Mathius <mathiusjohnsondeveloper@gmail.com>
2022-04-03 20:11:57 -0700

    add User page to routes

---
+++ src/pages/profile/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import UserPage from '../../features/users/UserPage'
+
+const index = () => {
+  return (
+    <div>
+      <UserPage></UserPage>
+    </div>
+  )
+}
+
+export default index
