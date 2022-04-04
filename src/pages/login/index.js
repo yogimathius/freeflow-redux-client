67305f04edd02d1ff0b28b4f28216e63dff9d39a
Mathius <mathiusjohnsondeveloper@gmail.com>
2022-04-03 20:14:50 -0700

    update login page route

---
+++ src/pages/login/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import LoginPage from '../../features/login/LoginPage'
+
+const index = () => {
+  return (
+    <div>
+      <LoginPage></LoginPage>
+    </div>
+  )
+}
+
+export default index
