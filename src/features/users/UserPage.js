1e7615b2d543bd75eb880222a916fd4677317e97
Mathius <info@yogimathius.dev>
2023-03-09 21:23:47 -0800

    fix: user page re render from crashing

--- src/features/users/UserPage.js
+++ src/features/users/UserPage.js
@@ -1,22 +1,38 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
-import { selectPostsByUser } from '../posts/reducers/postsSlice'
-import { selectHelperExperiencesByUserId } from '../../reducers/experiencesSlice'
+import React, { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { fetchPosts, selectPostsByUser } from '../posts/reducers/postsSlice'
+import { selectHelperExperiencesByUserId, fetchExperiences } from '../../reducers/experiencesSlice'
 import { loadState } from '../../helpers/localStorage'
-import { selectUserById } from '../../reducers/usersSlice'
+import { selectUserById, fetchUsers } from '../../reducers/usersSlice'
 import PostExcerpt from '../posts/components/PostExcerpt/PostExcerpt'
 import UserCard from './components/UserCard/UserCard'
-// import AddPostForm from '../posts/AddPostForm';
-// import {Link} from 'react-router-dom'
 
 export default function UserPage () {
   const loggedInUser = loadState()
+  const dispatch = useDispatch()
+
   const userId = loggedInUser.id
-  const postsForUser = useSelector((state) => selectPostsByUser(state, userId))
+
+  const experienceStatus = useSelector((state) => state.experiences.status)
+  const usersStatus = useSelector((state) => state.users.status)
+  const postsStatus = useSelector((state) => state.posts.status)
+  const userError = useSelector((state) => state.users.error)
+
+  useEffect(() => {
+    if (experienceStatus === 'idle') {
+      dispatch(fetchExperiences())
+    }
+    if (usersStatus === 'idle') {
+      dispatch(fetchUsers())
+    }
+    if (postsStatus === 'idle') {
+      dispatch(fetchPosts())
+    }
+  }, [experienceStatus, dispatch])
 
   const user = useSelector(state => selectUserById(state, userId))
-
   const experiencesForUser = useSelector((state) => selectHelperExperiencesByUserId(state, userId))
+  const postsForUser = useSelector((state) => selectPostsByUser(state, userId))
 
   if (!userId || !postsForUser || !experiencesForUser) {
     return null
@@ -28,19 +44,25 @@ export default function UserPage () {
     <PostExcerpt key={index} postId={post.id} index={index} />
   )
 
-  return (
-    <section className="space-y-4 pt-2 mx-4">
-      <div className="w-1/2">
-        <UserCard
-          user={user}
-        />
-      </div>
-      <div className="">
-        <h2 className="text-2xl font-semibold text-center border-2 border-green-500 rounded-xl bg-green-500 text-white">Timeline: </h2>
-      </div>
-      <section className="">
-        {renderedPosts}
+  if (usersStatus === 'idle') {
+    return <div className="loader">Loading...</div>
+  } else if (usersStatus === 'succeeded') {
+    return (
+      <section className="space-y-4 pt-2 mx-4">
+        <div className="w-1/2">
+          <UserCard
+            user={user}
+          />
+        </div>
+        <div className="">
+          <h2 className="text-2xl font-semibold text-center border-2 border-green-500 rounded-xl bg-green-500 text-white">Timeline: </h2>
+        </div>
+        <section className="">
+          {postsStatus === 'succeeded' ? renderedPosts : <div>Loading...</div>}
+        </section>
       </section>
-    </section>
-  )
+    )
+  } else if (usersStatus === 'failed') {
+    return <div>{userError}</div>
+  }
 }
