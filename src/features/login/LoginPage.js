b7ec43601522f1cc694189b4fbecd6e3a02f3759
Mathius <info@yogimathius.dev>
2023-03-12 21:29:09 -0700

    chore: add margin top login page

--- src/features/login/LoginPage.js
+++ src/features/login/LoginPage.js
@@ -15,7 +15,7 @@ export default function LoginPage () {
   }
 
   return (
-    <div className="text-center space-y-3 bg-white flex flex-col justify-center items-center h-96">
+    <div className="text-center space-y-3 flex flex-col justify-center items-center h-96 mt-16">
       <h1 className="text-2xl">Please login to view content.</h1>
       <LoginForm handleLogin={handleLogin} />
 
