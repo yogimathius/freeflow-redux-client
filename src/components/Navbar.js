b1ccd3a578773feebeb90da60da0583b331c099d
Mathius <info@yogimathius.dev>
2023-03-12 21:40:58 -0700

    fix: small screen responsiveness

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,6 +6,7 @@ import { saveState } from '../helpers/localStorage'
 import logo from '../images/logo.png'
 import { emptySkillsDB } from '../reducers/selectedSkillsSlice'
 import ProgressBar from './ProgressBar/ProgressBar'
+import DropDown from './DropDown'
 
 export const Navbar = ({ experience }) => {
   const { user } = useSelector(state => state.user)
@@ -46,11 +47,11 @@ export const Navbar = ({ experience }) => {
       </nav>
       <aside id="default-sidebar" className="fixed top-10 bg-gray-50 left-0 z-40 h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
         <section className="h-full px-8 py-4 bg-gray-50 dark:bg-gray-800">
-            {/* <div className="col-start-10 lg:hidden z-50 flex justify-end items-center mr-4">
+            <div className="lg:hidden z-50 ml-4 mt-8">
               <DropDown user={user} saveState={saveState} handleLogout={handleLogout} />
-            </div> */}
+            </div>
 
-            <div className="h-full px-3 py-4 overflow-y-auto dark:bg-gray-800">
+            <div className="hidden lg:block h-full px-3 py-4 overflow-y-auto dark:bg-gray-800">
               <Link onClick={() => (onDashboardClicked())} className="" to="/dashboard">
                 <div href="#" className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
                   <svg aria-hidden="true" className="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z"></path><path d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z"></path></svg>
