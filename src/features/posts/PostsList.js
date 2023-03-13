a2eea27f1d29806ab832eeab0746132780510085
Mathius <info@yogimathius.dev>
2023-03-12 22:43:57 -0700

    chore: make modal closable

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -1,15 +1,15 @@
 /* eslint-disable react/prop-types */
-import React from 'react'
+import React, { useState } from 'react'
 import { useSelector, connect } from 'react-redux'
 import PostExcerpt from './components/PostExcerpt/PostExcerpt'
 
 import Filter from '../filters/Filter'
 import { onCancelDelete, onConfirmDelete, onEdit, onSaveEdit } from './utils/visualModeTransitions'
 
-const PostsList = ({ posts, loggedInUser, openModal }) => {
+const PostsList = ({ posts, loggedInUser, openModal, isDisabled }) => {
   const postStatus = useSelector((state) => state.posts.status)
   const postError = useSelector((state) => state.posts.error)
-  console.log(openModal)
+
   let content
 
   if (postStatus === 'loading') {
@@ -34,7 +34,14 @@ const PostsList = ({ posts, loggedInUser, openModal }) => {
   return (
     <div className="mx-4 mt-16 py-4">
       <div className='bg-white py-4 rounded-lg border-1 border-gray-300'>
-        <input type="text" id="first_name" className="cursor-pointer border border-gray-400 text-gray-900 text-sm rounded-xl focus:ring-blue-500 focus:border-blue-500 block w-1/2 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 mx-auto" placeholder="Add a post..." onClick={() => openModal()}/>
+        <input
+          type="text"
+          id="first_name"
+          className="cursor-pointer border border-gray-400 text-gray-900 text-sm rounded-xl focus:ring-blue-500 focus:border-blue-500 block w-1/2 p-2.5 mx-auto pointer-events-auto"
+          placeholder="Add a post..."
+          onClick={() => openModal()}
+          disabled={isDisabled}
+          />
       </div>
       <Filter />
       {posts.length !== 0
