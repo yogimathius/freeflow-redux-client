3ce8f0251b6eceb8d46a315bef9dfb700fc5b350
Mathius <info@yogimathius.dev>
2023-03-13 23:40:04 -0700

    chore: add axios instance

--- src/axiosInstance.js
+++ src/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8080'
+  baseURL: process.env.REACT_APP_API_BASE_URL
 })
 
 axiosInstance.interceptors.request.use(
