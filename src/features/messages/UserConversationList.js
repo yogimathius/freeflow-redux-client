95cdedfc63a757ca1ca30188bb7dd72ef3164474
Mathius <info@yogimathius.dev>
2023-03-14 22:48:11 -0700

    chore: test

--- src/features/messages/UserConversationList.js
+++ src/features/messages/UserConversationList.js
@@ -26,7 +26,9 @@ const UserConversationList = () => {
   const { mode, transition } = useVisualMode(SHOW)
   const { path, url } = useRouteMatch()
   const { id } = loadState()
-  const conversationsStatus = useSelector((state) => state.userConversations.status)
+  const conversationsStatus = useSelector(
+    (state) => state.userConversations.status
+  )
 
   function onComposeMessage () {
     transition(COMPOSE)
@@ -39,12 +41,14 @@ const UserConversationList = () => {
   const handleOnClickLink = (messagerName) => {
     dispatch(setCurrentThread(messagerName))
   }
-
-  const conversations = useSelector((state) => state.userConversations.userConversations)
+  // test
+  const conversations = useSelector(
+    (state) => state.userConversations.userConversations
+  )
   const currentThread = useSelector((state) => state.currentThread)
   return (
-      <div className="grid grid-cols-4 grid-rows-8 mx-4">
-          {/* {mode === SHOW && (
+    <div className="grid grid-cols-4 grid-rows-8 mx-4">
+      {/* {mode === SHOW && (
 
             <div className="row-start-1 flex justify-center my-8 ml-4">
               <button className="btn btn-primary " onClick={onComposeMessage}>
@@ -52,47 +56,45 @@ const UserConversationList = () => {
               </button>
             </div>
           )} */}
-          {/* {mode === COMPOSE && ( */}
-            <div className="col-span-4 grid grid-cols-4 items-center my-7">
-              {/* <div className="flex justify-center"> */}
-              {/* <button onClick={() => transition(SHOW)} className="btn btn-warning col-span-1">Cancel</button> */}
-              {/* </div> */}
-              <UsernameSelector
+      {/* {mode === COMPOSE && ( */}
+      <div className="col-span-4 grid grid-cols-4 items-center my-7">
+        {/* <div className="flex justify-center"> */}
+        {/* <button onClick={() => transition(SHOW)} className="btn btn-warning col-span-1">Cancel</button> */}
+        {/* </div> */}
+        <UsernameSelector
+          userId={id}
+          messagers={conversations?.messagers}
+          currentThread={currentThread}
+          setCurrentThread={handleOnClickLink}
+          url={url}
+          path={path}
+        />
+      </div>
+      {/* )} */}
+
+      <MessagerNameList
+        currentThread={currentThread}
+        setCurrentThread={handleOnClickLink}
+        url={url}
+        userConversationNames={conversations?.messagers}
+        userId={id}
+      />
+
+      {mode === SHOW && (
+        <div className="row-start-3 row-span-7 col-start-2 col-span-3 ">
+          <Switch>
+            <Route path={`${path}/:messagerId`}>
+              <SelectedUserConversation
+                sortedMessages={conversations?.messages}
                 userId={id}
-                messagers={conversations?.messagers}
-                currentThread={currentThread}
-                setCurrentThread={handleOnClickLink}
-                url={url}
-                path={path}
               />
-            </div>
-          {/* )} */}
-
-          <MessagerNameList
-            currentThread={currentThread}
-            setCurrentThread={handleOnClickLink}
-            url={url}
-            userConversationNames={conversations?.messagers}
-            userId={id}
-          />
-
-          {mode === SHOW && (
-            <div className="row-start-3 row-span-7 col-start-2 col-span-3 ">
-              <Switch>
-                <Route path={`${path}/:messagerId`}>
-                  <SelectedUserConversation
-                    sortedMessages={conversations?.messages} userId={id}
-                  />
-                </Route>
-              </Switch>
-            </div>
-          )}
-
-          {mode === COMPOSE && (
-            <CreateMessageForm />
-          )}
-
+            </Route>
+          </Switch>
         </div>
+      )}
+
+      {mode === COMPOSE && <CreateMessageForm />}
+    </div>
   )
 }
 
