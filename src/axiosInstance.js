d4d74a5529f4ccc0e2bbfaf9f221dfdbc5bb8330
Mathius <info@yogimathius.dev>
2023-03-14 23:30:52 -0700

    chore: add freeflow-network domain

--- src/axiosInstance.js
+++ src/axiosInstance.js
@@ -1,7 +1,10 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL
+  baseURL:
+    process.env.NODE_ENV === 'production'
+      ? process.env.REACT_APP_API_BASE_URL
+      : 'http://localhost:8080/'
 })
 
 axiosInstance.interceptors.request.use(
